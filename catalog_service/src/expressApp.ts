2757f7ea04207de2492742e63e84b651b8386a79
nghiemxuan <nghiemxuan2006@gmail.com>
2024-10-30 23:53:17 +0700

    update port

--- catalog_service/src/expressApp.ts
+++ catalog_service/src/expressApp.ts
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(httpLogger);
 
+app.get("/health", (req, res) => {
+    return res.status(200).json({ message: "Catalog service is up" });
+})
+
 app.use("/", catalogRouter);
 
 app.use(HandleErrorWithLogger);
